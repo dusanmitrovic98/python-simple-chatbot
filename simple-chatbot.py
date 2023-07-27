dbd5ffcb52824de926bd80e72fa36483ab6852b6
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-27 22:14:57 +0200

    Update simple-chatbot.py

--- simple-chatbot.py
+++ simple-chatbot.py
@@ -72,3 +72,4 @@ def generate_random_response():
         "I'm an AI, but I still have my limitations.",
         "I'm just a friendly chat friend, not a human expert, so I may not know everything.",
         "I'm still learning, so bear with me!", "My knowledge is expanding every day!",
+        "I'm constantly being updated with new information.",
