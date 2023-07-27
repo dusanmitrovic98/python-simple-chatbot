bac5d1f8f506e857003e9fea52f8a4db609190b1
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-27 22:01:20 +0200

    Update simple-chatbot.py

--- simple-chatbot.py
+++ simple-chatbot.py
@@ -21,3 +21,4 @@ def generate_random_response():
         "I'm great, thanks!", "I'm always here, ready to chat!",
         "I'm alive and ready to chat!", "I'm doing well, thank you for asking.",
         "I'm doing splendidly!", "I'm feeling excellent!", "I'm doing quite well, how about you?",
+        "I'm doing just fine, thanks for checking!", "I'm here, ready to assist you 24/7!"
