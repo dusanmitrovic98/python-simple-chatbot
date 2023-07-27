aceee8b21194c0f61fedeeadc9f5aff04118faa9
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-27 21:57:39 +0200

    Update simple-chatbot.py

--- simple-chatbot.py
+++ simple-chatbot.py
@@ -8,3 +8,4 @@ def generate_random_response():
         "Hey, nice to meet you!", "Hi, what can I do for you?", "Hello, how can I assist you today?",
         "Hi, how are you doing?", "Hello, what's up?", "Hi there, how may I help you?",
         "Hey, it's good to see you!", "Hello, what brings you here?",
+        "Hi, what's happening?", "Hello, how's your day going?",
